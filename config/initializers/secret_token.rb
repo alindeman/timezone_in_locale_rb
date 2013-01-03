5213217f4bcfb8f547f49d1a50513474bdf624ba
Andy Lindeman <alindeman@gmail.com>
2013-01-02 21:17:23 -0500

    Initial Rails app generated via `rails new`

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TimezoneInLocaleRb::Application.config.secret_key_base = '93a55decd9e8e4dadb47b426bfec811bb11f912950b5e084d7af259985e7222005fbc8f83c8112ec72b9359420ca58924a1277bcd17f1228dd1aaf6341a2b429'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TimezoneInLocaleRb::Application.config.session_store :encrypted_cookie_store, key: '_timezone_in_locale_rb_session'
